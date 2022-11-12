7e64dd65a8f318e5c88e973ee4c1ad30ff61275a
RautureauClement <clement.rautureau@epitech.eu>
2022-11-12 17:27:07 +0100

    map string string

--- dashboard/api/src/main/java/com/dashboard/api/Error/WidgetResponseError.java
+++ dashboard/api/src/main/java/com/dashboard/api/Error/WidgetResponseError.java
@@ -2,8 +2,8 @@ package com.dashboard.api.Error;
 
 import java.util.Map;
 
-public class WidgetResponseError extends GenericResponseError<Map> {
-    public WidgetResponseError(Map errors) {
+public class WidgetResponseError extends GenericResponseError<Map<String, String>> {
+    public WidgetResponseError(Map<String, String> errors) {
         super();
         this.errorTypeValue = "WidgetError";
         this.errorContentsValue = errors;
