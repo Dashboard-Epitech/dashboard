4eb56bb3737f31aa4fb60a8e50d0c2482d2f26d3
RautureauClement <clement.rautureau@epitech.eu>
2022-11-12 17:19:43 +0100

    sup import

--- dashboard/api/src/main/java/com/dashboard/api/Controller/WeatherController.java
+++ dashboard/api/src/main/java/com/dashboard/api/Controller/WeatherController.java
@@ -1,25 +1,15 @@
 package com.dashboard.api.Controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dashboard.api.Error.BadUserFormResponseError;
-import com.dashboard.api.Error.WidgetResponseError;
 import com.dashboard.api.Request.WeatherRequest;
 import com.dashboard.api.Service.WeatherService;
 
