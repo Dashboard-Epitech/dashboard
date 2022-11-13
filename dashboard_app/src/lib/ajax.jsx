b68425ea91c46ba2228ed544b93ed6f99120215f
gdenis-epitech <gregoire.denis@epitech.eu>
2022-11-13 15:46:56 +0100

    feat: random em track

--- dashboard_app/src/lib/ajax.jsx
+++ dashboard_app/src/lib/ajax.jsx
@@ -16,14 +16,6 @@ export const authRegister = (username, email, password) => {
     })
 }
 
-export const authSpotify = (token) => {
-    const headers = {
-        'Authorization': 'Bearer ' + token,
-    }
-
-    return axios.get(BASE_URL + 'spotify/login', {headers})
-}
-
 export const getUserData = (email, token) => {
     const headers = {
         'Authorization': 'Bearer ' + token,
@@ -40,7 +32,21 @@ export const getWeather = (city) => {
     return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=248f71af2576da84a291a06fba0a8fdd`)
 }
 
+//GITHUB
+
+export const authGithub = () => {
+    return axios.get(BASE_URL + 'github/login');
+}
+
 //SPOTIFY 
+
+export const authSpotify = (token) => {
+    const headers = {
+        'Authorization': 'Bearer ' + token,
+    }
+
+    return axios.get(BASE_URL + 'spotify/login', {headers})
+}
 
 export const getSpotifyToken = (token) => {
     return axios.get(BASE_URL + 'spotify/usertoken', {
@@ -68,6 +74,14 @@ export const getUserPlaylists = (token) => {
 
 export const getPlaylistTracks = (token, playlistId) => {
     return axios.get(BASE_URL + 'spotify/playlists/tracks/' + playlistId, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+}
+
+export const getRandomEminemTrack = (token) => {
+    return axios.get(BASE_URL + 'spotify/track/eminem', {
         headers: {
             Authorization: `Bearer ${token}`
         }
